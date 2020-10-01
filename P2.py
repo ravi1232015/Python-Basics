1b8be0fa50d9fc81a03feec212377b23d5218aa1
ravi kumar <56681238+ravi1232015@users.noreply.github.com>
2020-10-01 10:37:59 +0530

    improve code

--- P2.py
+++ P2.py
@@ -1,6 +1,7 @@
 num1 = int(input("Enter first number : "))
 num2 = int(input("Enter second number : "))
-print("Sum : "+str(num1+num2))
+Sum = num1+num2
+print("Sum :",Sum)
 
 
 '''                Output
